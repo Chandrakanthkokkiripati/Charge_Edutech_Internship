db93b40dce56e23291d3149632ced39bf750b672
chandrakanth <chandrakanthkokkiripati1998@gmail.com>
2023-04-15 18:50:27 +0530

    modified files

--- index.js
+++ index.js
@@ -2,24 +2,54 @@ const temparatureButtonEl = document.getElementById("temparatureButton");
 const temparatureValueEl = document.getElementById("temparatureValue");
 let messageEl = document.getElementById("message");
 let inputTemparatureEl = document.getElementById("inputTemparature");
-
+const resetBtnEl = document.getElementById("resetBtn");
+const errorMsgEl = document.getElementById("error")
 
 temparatureButtonEl.addEventListener("click", function(event){
-    const convertedTemparatureValue = parseInt(inputTemparatureEl.value);
-    if (typeof(convertedTemparatureValue) === typeof(4)){
+    errorMsgEl.textContent = "";
+    messageEl.textContent = "";
+    const celToFah = (temp)=>{
+        return ((temp - 32) * (5/9)).toFixed(2) ;
+    }
+    const celToKel = (temp)=>{
+        let kelvinTemp = (temp - 273.15).toFixed(2);
+        return kelvinTemp
+    }
+    if (inputTemparatureEl.value === ""){
+        alert("Enter a valid input!!");
+    }else{
         if(temparatureValueEl.value === "Celsius"){
-            messageEl.textContent = inputTemparatureEl.value + " °C";
+            let result = (inputTemparatureEl.value) * 1;
+            if (isNaN(result) === false){
+                messageEl.textContent = result+ " \u00B0C";
+            }else{
+                errorMsgEl.textContent = "Enter a valid Number";
+            }
         }
         else if(temparatureValueEl.value === "Fahrenheit"){
-            let fahrenheitTemp = (convertedTemparatureValue-32) *(5/9);
-            messageEl.textContent = fahrenheitTemp +" °C";
+            let result = celToFah(inputTemparatureEl.value);
+            if (result === "NaN"){
+                errorMsgEl.textContent = "Enter a valid Number";
+            }else{
+                messageEl.textContent = celToFah(inputTemparatureEl.value) + " \u00B0C";
+            }
+            
         }
         else if(temparatureValueEl.value === "Kelvin"){
-            let kelvinTemp = convertedTemparatureValue - 273.15;
-            messageEl.textContent = kelvinTemp + " °C";
+            let result = celToKel(inputTemparatureEl.value);
+            if ((result) === "NaN"){
+                errorMsgEl.textContent = "Enter a valid Number";
+            }else{
+                messageEl.textContent = celToKel(inputTemparatureEl.value) + " \u00B0C";
+            }
+            
         }
     }
-    else{
-        alert("Enter a valid input");
-    }
+});
+
+
+resetBtnEl.addEventListener("click", function(event){
+    inputTemparatureEl.value = "";
+    messageEl.textContent = "";
+    errorMsgEl.textContent ="";
 });
